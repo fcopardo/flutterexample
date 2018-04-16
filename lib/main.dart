ba8958a55b70554f6d4773271d6e7c87a9d6b256
Fco Pardo <Francisco.pardo.baeza@gmail.com>
2018-04-16 13:34:11 -0300

    added iphone support, testing routing

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new RandomWords(),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new RandomWords(),
+        ),
+      ),
     );
   }
 }
+
+
+/*class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      //home: new RandomWords(),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new FlatButton(
+            child: const Text('FLAT BUTTON'),
+            onPressed: () {
+              Navigator.of(context).push(
+                new MaterialPageRoute(
+                  builder: (context) {
+                    return RandomWords();
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}*/
